6c3260f06140e3260a8d377f22b0d7c6a01b8f7c
axel-essono <naxelcdric@gmail.com>
2023-06-10 14:29:37 +0200

    Setting application for hilt di

--- app/src/main/java/com/axel/thesportsdb/TheSportDbAplication.kt
+++ app/src/main/java/com/axel/thesportsdb/TheSportDbAplication.kt
@@ -1,4 +1,8 @@
 package com.axel.thesportsdb
 
-class TheSportDbAplication {
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TheSportDbAplication : Application() {
 }
